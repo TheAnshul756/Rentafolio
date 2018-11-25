df70e11358afd8ccc160cc67fad75db28aca42b5
Amritansh Singhal <amritansh.singhal.cse17@itbhu.ac.in>
2018-11-25 12:24:16 +0530

    Updated views

--- home/models.py
+++ home/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+class Book(models.Model):
+    title=models.CharField(max_length=50)
+    author=models.CharField(max_length=50)
+    genre=models.CharField(max_length=50)
+    description=models.TextField(null=True)
+    mrp=models.PositiveIntegerField()
+    rating=models.FloatField(default=0.0)
+    class Meta:
+        ordering=('title',)
+    def __str__(self):
+        return self.title+" by "+self.author
+
+class BookInstance(models.Model):
+    book=models.ForeignKey(Book,on_delete=models.CASCADE)
+    b_date=models.DateField(null=True)
+    status=models.BooleanField(default=False)
+    borrower=models.ForeignKey('Profile',related_name='borrowed',null=True,on_delete=models.SET_NULL)
+    uploader=models.ForeignKey('Profile',related_name='uploaded',null=True,on_delete=models.SET_NULL)
+    def __str__(self):
+        return self.book_id+" , "+str(self.b_date)
+    
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    balance=models.FloatField(default=0.0)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Rating(models.Model):
+    user=models.OneToOneField(Profile,on_delete=models.CASCADE)
+    book=models.OneToOneField(Book,on_delete=models.CASCADE,related_name='rating_set')
+    rating=models.PositiveIntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
--- home/views.py
+++ home/views.py
@@ -1,9 +1,90 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404.HttpResponseRedirect
+from .models import *
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from functools import reduce
+from instamojo_wrapper import Instamojo
+from home import API_KEY,AUTH_TOKEN
+from datetime import datetime
+api = Instamojo(api_key=API_KEY, auth_token=AUTH_TOKEN, endpoint='https://test.instamojo.com/api/1.1/')
+
 # Create your views here.
 def index(request):
-    
-    return render(request,'home/index.html')
+    return HttpResponse("HomePage-OK")
+
+def bookDetailView(request,bid):
+    bk=get_object_or_404(Book,pk=bid)
+    return HttpResponse(bk.title)
+
+def catalogView(request):
+    if request.method=="GET":
+        pass
+    else:
+        books=Book.objects.all()
+        p=""
+        for i in books:
+            p+="<p>"+book.title"</p>"
+        return HttpResponse(p)
+
+def paymentView(request):
+    bid=request.session.get("book_id",-1)    
+    if(bid==-1):
+        return Http404
+    if request.method=="POST":
+        topay=Book.objects.get(id=bid).mrp
+        balance=request.user.profile.balance
+        if 'usebal' in request.POST:
+            if(balance>Book.objects.get(id=bid).mrp):
+                topay=0
+            else:
+                topay-=balance
+        if(topay>0):
+            if(topay<10):
+                topay=10
+            response = api.payment_request_create(
+                        amount=str(topay),
+                        purpose="Rentafolio Book Rental",
+                        send_email=False,
+                        email=request.user.email,
+                        buyer_name=request.user.username,
+                        phone=request.user.profile.phone,
+                        redirect_url=request.build_absolute_uri(reverse("checkout")),
+                    )    
+            return HttpResponseRedirect(response['payment_request']['longurl'])
+        else:
+            usr=request.user.profile
+            usr.balance-=Book.objects.get(id=bid).mrp
+            usr.save()
+        book_instances=Book.bookinstance_set.all()
+        for i in book_instances:
+            if(i.status==1):
+                i.borrower=request.user.profile
+                i.b_date=datetime.now()
+                i.status=0
+                i.save()
+                break
+        request.session["book_issued",bid]
+        return HttpResponseRedirect(reverse('checkout'))
+    return HttpResponse("PaymentPage-OK")
+
+        
+
+def profileView(request):
+    return HttpResponse("profileView-OK")
+
+def issuedView(request):
+    if request.method=="POST":
+        return_id=int(request.POST['return_id'])
+        return_book=BookInstance.objects.get(id=return_id)
+        return_book.borrower=None
+        return_book.status=1
+        return_book.save()
+        messages.warning(request,"Book successfully returned")
+    issued_books=request.user.profile.borrowed.all()
+    p=""
+    if len(issued_books==0):
+        return HttpResponse("You dont have any books issued")
+    else:
+        for i in issued_books:
+            p+="<p>"+i.book.title+" issued on "+i.b_date+"</p>"
+        return HttpResponse(p)
+
