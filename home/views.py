55835750b3b0e441b422c86ab6d981953262c923
Anshul Asawa <anshulasawa2011@gmail.com>
2018-11-26 19:21:57 +0530

    just a small change

--- home/views.py
+++ home/views.py
@@ -197,7 +197,7 @@ def issuedView(request):
         return_book.save()
         # days_issued=(datetime.now().date()-return_b1
         # ook.b_date.date())
-        abc=return_book.date
+        abc=return_book.b_date
         datetime.combine(abc, datetime.min.time())
         days_issued=datetime.now()-return_book.b_date
         days_issued=days_issued.days
